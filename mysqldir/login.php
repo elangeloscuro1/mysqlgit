542e2a8dfde4c46aaf2d2aac60bbc3fc9b63d272
elangeloscuro1 <angeltapia07084759@gmail.com>
2018-09-04 08:57:16 -0700

    commit

--- mysqldir/login.php
+++ mysqldir/login.php
@@ -1,7 +1,7 @@
 <?php // login.php
 $db_hostname = 'localhost';
 $db_database = 'angel_miracosta_fall2018';//'publications';
-$db_username = 'root';//'jim';
-$db_password = 'Anta4759';//'mypasswd';
+$db_username = '';//'jim';
+$db_password = '';//'mypasswd';
 ?>
 
